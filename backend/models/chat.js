ed15a67f9e60c64c5d61a989937ba4e476280180
Roth <rothvann2000@gmail.com>
2020-03-29 09:39:55 +0000

    Silly mistake

--- backend/models/chat.js
+++ backend/models/chat.js
@@ -21,137 +21,6 @@ function createChannel(members) {
 
 function getChannels(req, res) {
     knex('users').select(['channels', 'pending_channels']).where({username: req.user}).then(allChannels => {
-        console.log(allChannels);
-        if(allChannels[0]['pending_channels'].length != 0 || allChannels[0]['channels'].length != 0) {
-            knex('channels').select('*').where(knex.raw("channelid in ('" + allChannels[0]['pending_channels'].join("','") + "')")).then(result => {
-                
-                console.log(result);
-                for(let i = 0; i < result.length; i++) {
-                    let members = [...result[i]['members'], ...result[i]['pending_members']];
-                    members = members.filter((e) => {return e != req.user});
-                    if(members.length != 0) {
-                        pending_channels.push({channelId: result[i]['channelid'], members: members});
-                    }
-                }
-                res.json({channels: [...allChannels[0]['channels']], pending_channels: pending_channels});
-            }, result => {
-               console.log(result); 
-            });
-            
-            knex('channels').select('*').where(knex.raw("channelid in ('" + allChannels[0]['channels'].join("','") + "')")).then(result => {
-                let c = [];
-                console.log(result);
-                for(let i = 0; i < result.length; i++) {
-                    let members = [...result[i]['members'], ...result[i]['pending_members']];
-                    members = members.filter((e) => {return e != req.user});
-                    if(members.length != 0) {
-                        pending_channels.push({channelId: result[i]['channelid'], members: members});
-                    }
-                }
-                res.json({channels: [...allChannels[0]['channels']], pending_channels: pending_channels});
-            }, result => {
-               console.log(result); 
-            });
-        } else {
-            res.json(allChannels[0]);
-        }
-                
-    }, result => {
-        console.log(result);
-    });    
-}
-
-function joinChannel(req, res) {
-    knex('channels').update({
-        members: knex.raw("array_append(members, ?)", req.user),
-        pending_members: knex.raw("array_remove(pending_members, ?)", req.user)
-    }).where({channelid: req.body.channelId}).then(result => {
-        knex('users').update({
-            channels: knex.raw("array_append(channels, ?)", req.body.channelId),
-            pending_channels: knex.raw("array_remove(pending_channels, ?)", req.body.channelId)
-        }).where({username: req.user}).then(result => {
-            res.sendStatus(201);
-            console.log(result);
-        }, result => {
-            res.sendStatus(400);
-            console.log(result);
-        });
-    }, result => {
-        res.sendStatus(400);
-        console.log(result)
-    });
-}
-
-function leaveChannel(req, res) {
-     knex('channels').update({
-         //delete row
-        members: knex.raw("array_remove(members, ?)", req.user)
-    }).where({channelid: req.body.channelId}).then(result => {
-        knex('users').update({
-            channels: knex.raw("array_remove(channels, ?)", req.body.channelId),
-            pending_channels: knex.raw("array_remove(pending_channels, ?)", req.body.channelId),
-        }).where({username: req.user}).then(result => {
-            res.sendStatus(201);
-            console.log(result);
-        }, result => {
-            res.sendStatus(400);
-            console.log(result);
-        });
-    }, result => {
-        res.sendStatus(400);
-        console.log(result)
-    });
-    
-}
-
-function sendMessage(req, res) {
-    knex('messages').insert({
-        channelid: req.body.channelId,  
-        content: req.body.message,
-        date: (new Date()).toISOString()
-    }).then(result => {
-        res.sendStatus(201);
-        console.log(result); 
-    }, result => {
-        res.sendStatus(400);
-        console.log(result);
-    });
-}
-
-function getMessages(req, res) {
-    knex('messages').select('*').where({channelid: req.body.channelId}).then(result => {
-        res.status(201);
-        res.json({messages: result});
-        console.log(result);
-    }, result => {
-        res.sendStatus(400);
-        console.log(result);
-    });
-}
-
-module.exports = {createChannel, getChannels, sendMessage, getMessages, joinChannel, leaveChannel};var knex = require('./database');
-
-function createChannel(members) {
-    knex('channels').insert({members: []}).returning('channelid').then(result => {
-        console.log(result);
-        console.log(members[0]);
-        for(let i = 0; i < members.length; ++i) {
-            let user = members[i];
-            knex('users').update({
-                pending_channels: knex.raw("pending_channels || '{\"" + result.join("','") + "\"}'")
-            }).where({username: user}).then(result => {
-                console.log(result);
-            }, result => {
-                console.log(result);
-            });
-        }
-    }, result => {
-        console.log(result)
-    });
-}
-
-function getChannels(req, res) {
-    knex('users').select(['channels', 'pending_channels']).where({username: req.user}).then(allChannels => {
         let promises = [];
         let pending_channels = [];
         let channels = [];
@@ -250,6 +119,7 @@ function sendMessage(req, res) {
 }
 
 function getMessages(req, res) {
+    console.log('reee');
     knex('messages').select('*').where({channelid: req.body.channelId}).then(result => {
         res.status(201);
         res.json({messages: result});
